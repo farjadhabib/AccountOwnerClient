de37c0910dbc3b5caf99817f603f327d1472fc8f
Farjad Habib <habib.farjad@gmail.com>
2019-06-08 19:47:28 -0700

    Created not found, repository and environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router'
+import { HttpClientModule } from '@angular/common/http';
+
+import { EnvironmentUrlService } from './shared/services/environment-url.service';
+import { RepositoryService } from './shared/services/repository.service';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { OwnerComponent } from './owner/owner.component';
 import { MenuComponent } from './menu/menu.component';
+import { NotFoundComponent } from './error-pages/not-found/not-found.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     OwnerComponent,
-    MenuComponent
+    MenuComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot([
       { path: 'home', component: HomeComponent },
-      { path: '', redirectTo: '/home', pathMatch: 'full' }
+      { path: '404', component : NotFoundComponent},
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: '**', redirectTo: '/404', pathMatch: 'full'}
     ])
   ],
-  providers: [],
+  providers: [
+    EnvironmentUrlService,
+    RepositoryService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
