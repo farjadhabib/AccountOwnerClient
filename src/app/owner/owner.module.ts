58fd66178aa101f9a4baf3cc596a8dacba758f11
Farjad Habib <habib.farjad@gmail.com>
2019-06-08 20:00:51 -0700

    created owner list component

---
+++ src/app/owner/owner.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OwnerListComponent } from './owner-list/owner-list.component';
+ 
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [OwnerListComponent]
+})
+export class OwnerModule { }
