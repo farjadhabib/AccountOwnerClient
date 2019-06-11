531991744c746b6120214d18b71328e73df0bab9
Farjad Habib <habib.farjad@gmail.com>
2019-06-10 21:16:44 -0700

    Ready for app dynamics integration

--- src/app/owner/owner.module.ts
+++ src/app/owner/owner.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import { OwnerListComponent } from './owner-list/owner-list.component';
 import { OwnerDetailsComponent } from './owner-details/owner-details.component';
 import { SharedModule } from './../shared/shared.module';
+import { OwnerCreateComponent } from './owner-create/owner-create.component';
 
 @NgModule({
     imports: [
         CommonModule,
         SharedModule,
+        ReactiveFormsModule,
         RouterModule.forChild([
             { path: 'list', component: OwnerListComponent },
-            { path: 'details/:id', component: OwnerDetailsComponent }
+            { path: 'details/:id', component: OwnerDetailsComponent },
+            { path: 'create', component: OwnerCreateComponent }
         ])
     ],
     declarations: [
         OwnerListComponent,
-        OwnerDetailsComponent
+        OwnerDetailsComponent,
+        OwnerCreateComponent
     ]
 })
-export class OwnerModule { }
+export class OwnerModule { 
+    
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ErrorModalComponent } from './modals/error-modal/error-modal.component';
+import { SuccessModalComponent } from './modals/success-modal/success-modal.component';
+import { DatepickerDirective } from './datepicker.directive';
 
 @NgModule({
-  declarations: [],
+  declarations: [ErrorModalComponent, SuccessModalComponent, DatepickerDirective],
   imports: [
     CommonModule
   ],
-  exports: []
+  exports: [
+    ErrorModalComponent,
+    SuccessModalComponent,
+    DatepickerDirective
+  ]
 })
 export class SharedModule { }
